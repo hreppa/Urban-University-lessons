1f5c77b02ace246ce50ed99290703d13531f27d0
Пётр <khreptugov@gmail.com>
2024-12-27 02:56:55 +0300

    создал первый файл и коммит для него

---
+++ Урок_1.py
@@ -0,0 +1,38 @@
+# 1st program
+
+print(9**0.5*4)
+
+
+
+# 2nd program
+
+print(9.99 > 9.98 and 100 != 100.1)
+
+
+
+#3rd program
+
+result_1 = 2*2+2
+
+# print(result_1)
+
+result_2 = 2*(2+2)
+
+# print(result_2)
+
+print(result_1 == result_2)
+
+
+
+#4th program
+
+simbol = float("123.456")*10
+
+last_figure = int(simbol) % 10
+
+print(f"первая цифра после запятой - {last_figure}")
+
+
+#4th program вариант 2
+
+print(int(float("123.456")*10) % 10)
