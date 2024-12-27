c5f5f224de7adeb5cdefd45ac893b8b592a2f3a0
Пётр <khreptugov@gmail.com>
2024-12-27 03:20:38 +0300

    убрал лишние пробелы и добавил коммиты в строки 26 и 28

--- Урок_1.py
+++ Урок_1.py
@@ -3,14 +3,12 @@
 print(9**0.5*4)
 
 
-
 # 2nd program
 
 print(9.99 > 9.98 and 100 != 100.1)
 
 
-
-#3rd program
+# 3rd program
 
 result_1 = 2*2+2
 
@@ -23,12 +21,11 @@ result_2 = 2*(2+2)
 print(result_1 == result_2)
 
 
+#4th program вариант 1
 
-#4th program
+simbol = float("123.456")*10  # преобразовали текст в число с точкой и увеличили в 10 раз
 
-simbol = float("123.456")*10
-
-last_figure = int(simbol) % 10
+last_figure = int(simbol) % 10  # преобразовали десятичное число в целое и взяли остаток от деления на 10
 
 print(f"первая цифра после запятой - {last_figure}")
 
